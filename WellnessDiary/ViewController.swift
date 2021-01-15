bddee010d66a05f0da882a9bd812fc2b16c63460
Luis M. Flores <floresv.luism@gmail.com>
2021-01-15 15:08:45 -0500

    Project created

---
+++ WellnessDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellnessDiary
+//
+//  Created by luis flores on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
