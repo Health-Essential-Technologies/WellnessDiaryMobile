f582fff3d5640e715d08fe4af7e919f35cfed5ec
Luis M. Flores <floresv.luism@gmail.com>
2021-01-17 17:21:02 -0500

    simpletababr added

--- WellnessDiary/Model/TabViewController/SimpleTabViewController.swift
+++ WellnessDiary/Model/TabViewController/SimpleTabViewController.swift
@@ -22,8 +22,8 @@ class SimpleTabViewController: UITabBarController {
     override func loadBasicTheme() {
         let mainColor = UIColor(named: "mainColor")
         tabBar.barTintColor = mainColor
-//        tabBar.tintColor = ThemeKitManager.shared.getColorWith(name: ThemeKitManager.NAVIGATION_TABBAR_TITLE_TINT_COLOR_VAR_NAME)
-//        tabBar.unselectedItemTintColor = ThemeKitManager.shared.getColorWith(name: ThemeKitManager.NAVIGATION_TABBAR_UNSELECTED_ITEM_TINT_COLOR_VAR_NAME)
+      tabBar.tintColor = UIColor.white
+      tabBar.unselectedItemTintColor = UIColor.white
     }
 
 }
