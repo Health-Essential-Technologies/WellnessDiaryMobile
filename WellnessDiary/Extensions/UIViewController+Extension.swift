6217bf00387fdad04c7d2000b499b23f82d1a904
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-01-27 14:17:38 -0500

    Methods to add view controller

--- WellnessDiary/Extensions/UIViewController+Extension.swift
+++ WellnessDiary/Extensions/UIViewController+Extension.swift
@@ -10,6 +10,29 @@ import UIKit
 extension UIViewController {
     
     // MARK: - Methods
+  
+  /**
+   Adds a child view controller with a given frame. If no frame given, child view will inherence the frame from parent view frame.
+   */
+  @nonobjc func add(_ child: UIViewController, frame: CGRect? = nil) {
+    addChild(child)
+    if let frame = frame {
+      child.view.frame = frame
+    } else {
+      child.view.frame = view.frame
+    }
+    view.addSubview(child.view)
+    child.didMove(toParent: self)
+  }
+  
+  /**
+   Removes the viewcontroller from its parent
+   */
+  func remove() {
+    willMove(toParent: nil)
+    view.removeFromSuperview()
+    removeFromParent()
+  }
     
     @objc func loadBasicTheme() {
         view.backgroundColor = .white
