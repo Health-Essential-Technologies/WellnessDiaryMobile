57438f960b4b880072f9c07c6f7864a6395b9c29
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-01-27 20:16:47 -0500

    - Tint color added to main window

--- WellnessDiary/Resources/SceneDelegate.swift
+++ WellnessDiary/Resources/SceneDelegate.swift
@@ -8,15 +8,16 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        let mainBarVC = MainTabViewController()
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = mainBarVC
-        window?.makeKeyAndVisible()
-    }
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = scene as? UIWindowScene else { return }
+    let mainBarVC = MainTabViewController()
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = mainBarVC
+    window?.tintColor = Colors.mainColor.color
+    window?.makeKeyAndVisible()
+  }
 }
 
