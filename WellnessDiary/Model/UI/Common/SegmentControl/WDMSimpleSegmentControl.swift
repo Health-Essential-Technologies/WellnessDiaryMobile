e4db7564f449f80d097a380d327f8069c0785f45
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-07 22:10:05 -0500

    - Diagnostics settings added. - Hidden settings added. - Simple segment controls added

---
+++ WellnessDiary/Model/UI/Common/SegmentControl/WDMSimpleSegmentControl.swift
@@ -0,0 +1,14 @@
+//
+//  WDMSimpleSegmentControl.swift
+//  WellnessDiary
+//
+//  Created by luis flores on 3/5/21.
+//  Copyright © 2021 Health Essential Technologies. All rights reserved.
+//
+
+import UIKit
+
+class WDMSimpleSegmentControl: UISegmentedControl {
+
+   
+}
