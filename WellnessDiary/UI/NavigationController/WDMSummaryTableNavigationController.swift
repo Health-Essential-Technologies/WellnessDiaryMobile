fe436e200e696d341cefe6249d30a1b2741f1a16
Luis M. Flores <flores_luis_m@outlook.com>
2021-04-12 12:23:52 -0400

    json and random data load sent to background thread

---
+++ WellnessDiary/UI/NavigationController/WDMSummaryTableNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  WDMSummaryTableNavigationController.swift
+//  WellnessDiary
+//
+//  Created by Luis Flores on 4/11/21.
+//  Copyright © 2021 Health Essential Technologies. All rights reserved.
+//
+
+import UIKit
+
+class WDMSummaryTableNavigationController: ORKPasscodeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
